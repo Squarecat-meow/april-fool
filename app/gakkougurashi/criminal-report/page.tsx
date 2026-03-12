c6a747bb86c5ab0af17334038c45205ecd6a4a8d
Squarecat-meow <yozumina@serafuku.moe>
2026-03-13 06:48:55 +0900

    feat(pages): 각 페이지에 Noti, Browser 컴포넌트 추가

--- app/gakkougurashi/criminal-report/page.tsx
+++ app/gakkougurashi/criminal-report/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import Browser from "@/app/components/browser/browser";
+import Noti from "@/app/components/noti";
+import PageNavbar from "@/app/components/page-nav";
+import PageTitle from "@/app/components/page-title";
+import { usePathname } from "next/navigation";
+
 export default function Page() {
-  return <section></section>;
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="w-full relative">
+        <PageNavbar path={pathname} />
+      </div>
+      <PageTitle>학교 폭력･성범죄 신고함</PageTitle>
+      <section className="mt-6">
+        <Noti />
+        <article>
+          <Browser category={pathname} />
+        </article>
+      </section>
+    </>
+  );
 }
--- app/noti/wolha-press/page.tsx
+++ app/noti/wolha-press/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import Browser from "@/app/components/browser/browser";
+import Noti from "@/app/components/noti";
+import PageNavbar from "@/app/components/page-nav";
+import PageTitle from "@/app/components/page-title";
+import { usePathname } from "next/navigation";
+
 export default function Page() {
-  return <section></section>;
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="w-full relative">
+        <PageNavbar path={pathname} />
+      </div>
+      <PageTitle>월하고 언론 보도자료</PageTitle>
+      <section className="mt-6">
+        <Noti />
+        <article>
+          <Browser category={pathname} />
+        </article>
+      </section>
+    </>
+  );
 }
