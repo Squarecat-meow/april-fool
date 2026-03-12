4f66c1cbe20435ef5925ec6f042ea87fe6c2c8f5
Squarecat-meow <yozumina@serafuku.moe>
2026-03-13 03:57:47 +0900

    feat(navi): 약도 추가

--- app/introduct/navi/page.tsx
+++ app/introduct/navi/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import PageNavbar from "@/app/components/page-nav";
+import PageTitle from "@/app/components/page-title";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 export default function Page() {
-  return <section></section>;
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="w-full relative">
+        <PageNavbar path={pathname} />
+      </div>
+      <PageTitle>찾아오는 길</PageTitle>
+      <Image
+        src={"/map.jpg"}
+        alt="월하고등학교 약도"
+        width={1000}
+        height={500}
+        unoptimized
+        className="mt-6 mx-auto"
+      />
+    </>
+  );
 }
