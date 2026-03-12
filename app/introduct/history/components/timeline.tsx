c6938e8f211a7f2f1b845fc8bd091b76263f267d
Squarecat-meow <yozumina@serafuku.moe>
2026-03-13 03:45:21 +0900

    fix(history): Timeline 컴포넌트 좌우 나눌 수 있게, 페이지 최신화

--- app/introduct/history/components/timeline.tsx
+++ app/introduct/history/components/timeline.tsx
@@ -1,24 +1,33 @@
-const Timeline = ({
+type TDirection = "left" | "right";
+
+function Timeline({
   children,
   className,
 }: {
   children: React.ReactNode;
   className?: string;
-}) => {
-  return (
-    <ul className={`w-fit mx-auto space-y-12 ${className ?? ""}`}>
-      {children}
-    </ul>
-  );
-};
+}) {
+  return <ul className={`mx-auto ${className ?? ""}`}>{children}</ul>;
+}
 
-function Element({ children }: { children: React.ReactNode }) {
+function Element({
+  children,
+  direction = "right",
+}: {
+  children: React.ReactNode;
+  direction?: TDirection;
+}) {
   return (
-    <li className="flex items-center gap-2 relative">
-      <div className="w-5 h-5 bg-blue-500 rounded-full grid place-items-center">
+    <li className="grid grid-cols-[1fr_auto_1fr] items-center gap-4">
+      <div className={direction === "left" ? "text-right" : ""}>
+        {direction === "left" && children}
+      </div>
+      <div className="w-5 h-5 bg-blue-500 rounded-full grid place-items-center z-10">
         <div className="w-4 h-4 border-2 border-white rounded-full" />
       </div>
-      <div className="flex flex-col">{children}</div>
+      <div className={direction === "right" ? "text-left" : ""}>
+        {direction === "right" && children}
+      </div>
     </li>
   );
 }
@@ -33,7 +42,13 @@ function Text({ children }: { children: React.ReactNode }) {
 
 function Line() {
   return (
-    <span className="absolute top-8 left-[0.55em] h-20 border-r-2 border-blue-500" />
+    <li className="grid grid-cols-[1fr_auto_1fr] gap-4" aria-hidden>
+      <div />
+      <div className="w-5 flex justify-center">
+        <span className="h-12 border-r-2 border-blue-500" />
+      </div>
+      <div />
+    </li>
   );
 }
 
--- app/introduct/history/page.tsx
+++ app/introduct/history/page.tsx
@@ -14,19 +14,39 @@ export default function Page() {
       </div>
       <PageTitle>학교 연혁</PageTitle>
       <Timeline className="mt-6">
-        <Timeline.Element>
+        <Timeline.Element direction="right">
           <Timeline.Date>2023.9.3</Timeline.Date>
           <Timeline.Text>월하고등학교 개교</Timeline.Text>
-          <Timeline.Line />
         </Timeline.Element>
-        <Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="right">
           <Timeline.Date>2024.9.3</Timeline.Date>
           <Timeline.Text>월하고등학교 1주년</Timeline.Text>
-          <Timeline.Line />
         </Timeline.Element>
-        <Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="left">
+          <Timeline.Date>2024.12.10</Timeline.Date>
+          <Timeline.Text>로컬 노트 50,000개 돌파</Timeline.Text>
+        </Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="left">
+          <Timeline.Date>2025.2.6</Timeline.Date>
+          <Timeline.Text>로컬 노트 100,000개 돌파</Timeline.Text>
+        </Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="left">
+          <Timeline.Date>2025.10.27</Timeline.Date>
+          <Timeline.Text>학생 수 100명 돌파</Timeline.Text>
+        </Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="right">
           <Timeline.Date>2025.9.3</Timeline.Date>
           <Timeline.Text>월하고등학교 2주년</Timeline.Text>
+        </Timeline.Element>
+        <Timeline.Line />
+        <Timeline.Element direction="left">
+          <Timeline.Date>2026.1.12</Timeline.Date>
+          <Timeline.Text>로컬 노트 200,000개 돌파</Timeline.Text>
         </Timeline.Element>
       </Timeline>
     </>
