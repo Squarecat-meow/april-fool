e2f84badf14664202e6f09d679eebc7099b0d713
Squarecat-meow <yozumina@serafuku.moe>
2026-03-13 06:48:24 +0900

    feat(goals): 교육목표 이미지 추가

--- app/introduct/goals/page.tsx
+++ app/introduct/goals/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import PageNavbar from "@/app/components/page-nav";
+import PageTitle from "@/app/components/page-title";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 export default function Page() {
-  return <section></section>;
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="w-full relative">
+        <PageNavbar path={pathname} />
+      </div>
+      <PageTitle>교육목표</PageTitle>
+      <Image
+        src={"/goals.png"}
+        alt="월하고등학교 교육목표"
+        width={1000}
+        height={675}
+        unoptimized
+        className="mt-6 mx-auto"
+      />
+    </>
+  );
 }
