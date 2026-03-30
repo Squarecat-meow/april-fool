75d68687d3e25d1235bc4248caa6f211a9df57e7
Squarecat-meow <yozumina@serafuku.moe>
2026-03-30 19:01:29 +0900

    prismaPg adapter DB 주소 변경

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.POSTGRES_URL_NON_POOLING!,
+  connectionString: process.env.POSTGRES_PRISMA_URL!,
 });
 const prisma = new PrismaClient({ adapter });
 
