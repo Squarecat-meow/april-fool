349a589d8e5be20685eadd1c4c5ed53b1af13d85
Squarecat-meow <yozumina@serafuku.moe>
2026-03-30 18:57:39 +0900

    Prisma Client Adapter DB 주소 변경

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,10 @@
-import 'dotenv/config';
-import { PrismaClient } from '@/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({
+  connectionString: process.env.POSTGRES_URL_NON_POOLING!,
+});
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
