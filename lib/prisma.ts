3cc96af32c1777d1ede3b0dbe90ebcca8eb65137
Squarecat-meow <yozumina@serafuku.moe>
2026-03-30 19:04:26 +0900

    prismaPg adapter ssl 설정 변경

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
   connectionString: process.env.POSTGRES_PRISMA_URL!,
+  ssl: { rejectUnauthorized: false },
 });
 const prisma = new PrismaClient({ adapter });
 
